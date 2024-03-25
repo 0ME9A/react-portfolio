fd6403a682e6a85b3892feedd9f8d1f6bdd68da4
Baliram Singh <heyome9a@gmail.com>
2024-03-25 16:14:59 +0530

    test 10

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <h1>This is test of component</h1>
+    <h1>This is test of components</h1>
   </React.StrictMode>
 );
 
