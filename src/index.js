7b296a45aa3975bd64cba0228cb7ad781f850c8d
Baliram Singh <heyome9a@gmail.com>
2024-03-25 16:18:58 +0530

    test 11

--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
 import reportWebVitals from "./reportWebVitals";
 import ReactDOM from "react-dom/client";
 import React from "react";
-// import App from "./App";
+import App from "./App";
 import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
-    <h1>This is test of components</h1>
+    <App />
   </React.StrictMode>
 );
 
