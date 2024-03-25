8e5d09b33a6a9060571d4c3776fc3bfb6b3b1f83
Baliram Singh <heyome9a@gmail.com>
2024-03-25 15:45:08 +0530

    test 5

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
-import { createRoot } from "react-dom/client";
-import { StrictMode } from "react";
+import reportWebVitals from "./reportWebVitals";
+import ReactDOM from "react-dom/client";
+import React from "react";
 import App from "./App";
 import "./index.css";
 
-const root = createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <StrictMode>
+  <React.StrictMode>
     <App />
-  </StrictMode>
+  </React.StrictMode>
 );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
