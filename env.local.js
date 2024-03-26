f432ba30d7ab948653bbd5604b8bb38468583371
Baliram Singh <heyome9a@gmail.com>
2024-03-26 07:07:30 +0530

    Update env.local.js

--- env.local.js
+++ env.local.js
@@ -1,4 +1,4 @@
-// this is .env file rename the file name (.env.local), uncomment all variable and remove this comment.
+//This is a .env file rename the file name (.env.local), uncomment all variables and remove this comment. This is used for emailjs (send emails https://www.emailjs.com/docs/examples/reactjs/), so first create an account on emailjs then fill in the required field from emailjs.
 
 // REACT_APP_SERVICE_ID=
 // REACT_APP_TEMPLATE_ID=
